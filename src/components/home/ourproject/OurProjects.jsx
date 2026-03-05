1af764921228e5d1ca7b5a3ca8558a3526dd785b
Suraj Singh <surajsln570@gmail.com>
2026-03-05 20:49:15 +0530

    Fetch projects in OurProjects component
    Refactor OurProjects component to fetch projects directly within the function.

--- src/components/home/ourproject/OurProjects.jsx
+++ src/components/home/ourproject/OurProjects.jsx
@@ -1,8 +1,10 @@
 import Container from '@/components/Container'
 import OurProjImage from './comp/OurProjImage'
+import { getAllProject } from "@/fetch/project.api";
 
-export default function OurProjects({projects}) {
-
+export default function OurProjects() {
+const projects = await getAllProject();
+  console.log(projects)
   const CivilConstruction = projects.filter(
     p => p.projectType === 'Civil Construction'
   ).slice(0,3)
